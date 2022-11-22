fe7b00263ad495527cdbd749adc9db5e835f37d9
DANNY DALEY <dd252935@falmouth.ac.uk>
2022-11-22 13:29:35 +0000

    Built the nav bar for logged in users.

--- client/my-uni-social/src/App.js
+++ client/my-uni-social/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './Components/NavBar';
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
