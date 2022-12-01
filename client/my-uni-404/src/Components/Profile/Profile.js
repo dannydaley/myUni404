b7b180e48b2f0f58386184d61f409f60ec6e9c95
Danny Daley <dd252935@falmouth.ac.uk>
2022-12-01 14:55:52 +0000

    changed .env

--- client/my-uni-404/src/Components/Profile/Profile.js
+++ client/my-uni-404/src/Components/Profile/Profile.js
@@ -8,10 +8,29 @@ class Profile extends React.Component {
         super();
         this.state = {
             feed: "asked",
+            feedData: [],
         };
     }
     switchAsked = () => this.setState({ feed: "asked" });
     switchAnswered = () => this.setState({ feed: "answered" });
+
+    //COMPONENT DID MOUNT IS BUILT IN AND RUNS WHEN THE COMPONENT MOUNTS
+    componentDidMount = async () => {
+        // this.setState({ settings: newSettings })
+        //FETCH IS A GET REQUEST BY DEFAULT, POINT IT TO THE ENDPOINT ON THE BACKEND
+        fetch(process.env.REACT_APP_SERVER + "/getUserQuestionFeed", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                userID: this.props.userID,
+            }),
+        })
+            //TURN THE RESPONSE INTO A JSON OBJECT
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ feedData: data });
+            });
+    };
 
     render() {
         return (
@@ -49,6 +68,7 @@ class Profile extends React.Component {
                 <div>
                     {this.state.feed === "asked" ? (
                         <div>
+                            {}
                             <QuestionCard />
                             <QuestionCard />
                             <QuestionCard />
